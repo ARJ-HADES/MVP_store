5bad6f8923ed69cb3919de3c75fc5951593f0aa1
Arjun <68515819+ARJ-HADES@users.noreply.github.com>
2023-06-24 03:17:30 +0400

    Update product.js

--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -13,8 +13,8 @@ export default {
       }
     },
     { 
-      name: 'name',
-      title: 'Name',
+      name: 'productName',
+      title: 'Product Name',
       type: 'string',
     },
     { 
